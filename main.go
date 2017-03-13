3ccc4daf83223ada84b729518c7da7a0d5a0539c
Spencer Nelson <spencer@twitch.tv>
2017-03-13 18:45:44 -0400

    Add dependency on retool_test_lib

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-const version = "v1"
+	"github.com/spenczar/retool_test_lib"
+)
+
+const version = "v2"
 
 func main() {
+	_ = retool_test_lib.Exported
 	fmt.Println(version)
 }
